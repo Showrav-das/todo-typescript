f85a80e084aff6509f2d1939bf3b1f34fc62a636
Showrav-das <showravdas8@gmail.com>
2022-05-17 20:12:56 +0600

    first commit

---
+++ src/Components/TodoTask.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Task } from './Interface';
+
+
+interface props{
+    task: Task;
+    deleteTask(deleteTaskName: Number): void;
+    updated(updateTask: string, updateNo: number): void;
+    topHandle(top:string):any;
+}
+const TodoTask = ({ task,deleteTask,updated,topHandle}: props) => {
+    const {taskName,taskNo} = task;
+
+    return (
+        <div className='task-container'>
+           <p>{taskName} </p>
+            <p>{taskNo}</p>
+            <button onClick={() => deleteTask(taskNo)}>X</button>
+            <button onClick={() => updated(taskName, taskNo)}>Update</button>
+            <button onClick={()=>topHandle(taskName)}>Top</button>
+        </div>
+    );
+};
+
+export default TodoTask;
---
+++ src/Components/Todos.tsx
@@ -0,0 +1,90 @@
+import React, {FC, useState} from 'react';
+import {Task} from './Interface';
+import TodoTask from './TodoTask';
+import './main.css';
+
+/*interface Task{
+    taskName: string;
+    taskNo: number;
+}*/
+
+const Todos:FC = () => {
+    const [task, setTask] = useState<string>('');
+    const [taskNo, setTaskNo] = useState<number>(0);
+    const [todo, setTodo] = useState<Task[]>([]);
+   
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement>):void => {
+        if (e.target.name === "task") {
+            setTask(e.target.value);
+        }
+        else {
+            setTaskNo(Number(e.target.value));
+        }
+    }
+    const addTask = (): void => {
+        const newTodo = { taskName: task, taskNo: taskNo };
+        if (task==='' || taskNo===0) {
+            return;
+        }
+        else {
+            setTodo([...todo, newTodo]);
+            setTask('');
+            setTaskNo(0);
+        }  
+       
+    }
+
+    const deleteTask = (deleteTaskName:Number):void => {
+        setTodo(todo.filter(t => t.taskNo !== deleteTaskName));
+    }
+
+    const updated = (updateTask: string, updateNo: number): void => {
+        const find = todo.find(t => {
+            return t.taskName === updateTask;
+        });
+        //setTodo();
+        console.dir(find?.taskName);
+        //setTodo([find]);
+        setTask(updateTask);
+        setTaskNo(updateNo);
+        
+    }
+
+    const topHandle = (top:string):any => {
+        //const find = todo.find(t => {
+        //    return t.taskName === top;
+        //});
+        todo.forEach(single => {
+            //console.log(single);
+            if (single.taskName === top) {
+                setTodo([single,...todo]);
+                const a = (todo.lastIndexOf(single) + 1);
+               
+            }
+            
+        });
+        
+    }
+
+    return (
+        <div>
+           <div className="todo-input">
+           <h2>Todo list with typescript</h2>
+           <div style={{display:'flex',alignItems:"center", justifyContent:"center",gap:"10px"}}>
+           <p>Task Name</p>
+            <input type="text" value={task} name="task" onChange={handleChange} placeholder='enter your homework name'/>
+            <p>Task Number</p>
+            <input type="number" value={taskNo} onChange={handleChange}  placeholder='enter your home work no'/>
+           <button onClick={addTask}>Add Task</button>
+           </div>
+           </div>
+            <div>
+                {
+                    todo.map(task => <TodoTask task={task} deleteTask={deleteTask} updated={updated} topHandle={topHandle}/>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Todos;
